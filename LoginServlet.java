e9af1e669ca313c4706f9020587d5975f99bf7a3
GargamelPlus <32667791+GargamelPlus@users.noreply.github.com>
2017-10-10 17:40:56 +0800

    Add files via upload

---
+++ LoginServlet.java
@@ -0,0 +1,57 @@
+package com.qingshixun.login;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.jasper.tagplugins.jstl.core.Out;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet(description = "LoginServlet" , urlPatterns = {"/LoginServlet"})
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		String remeber = request.getParameter("remeberMe");//是否记住密码
+		if(username!=""&&password!=""){
+			request.getSession().setAttribute("username", username);//将用户保存到session
+			request.setAttribute("loginUser", username);
+			request.getSession().setAttribute("loginPassword", password);//将密码保存到session
+			response.sendRedirect("welcome.jsp");
+		}
+		else{
+			response.sendRedirect("login.jsp");
+		}
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
